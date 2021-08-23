ff6f7a4ad6a03eb2c052754fa8edb4434bbf203f
Willem Ruan <ruan.nieu@gmail.com>
2021-08-23 19:16:57 +0200

    custom form now opens and closes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let bookStorage = [];
 
+const form = document.querySelector("#bookInput");
+const openBtn = document.querySelector("#addBtn");
+const cancelBtn = document.querySelector("#cancelBtn");
+
 function book(){
 
 };
@@ -11,4 +15,18 @@ function storeBook(book){
 function displayBooks(){
 
 };
+
+function showForm(){
+    form.style.display = "grid";
+};
+
+function hideForm(){
+    form.style.display = "none";
+};
+
+openBtn.addEventListener('click', showForm)
+
+cancelBtn.addEventListener('click', () =>{
+    hideForm();
+});
 
