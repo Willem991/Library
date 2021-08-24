877f7b33eac060cb882fbb9c282c860f4506ac84
Willem Ruan <ruan.nieu@gmail.com>
2021-08-24 18:09:34 +0200

    added basic remove button functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const openBtn = document.querySelector("#addBtn");
 const cancelBtn = document.querySelector("#cancelBtn");
 const lockScreen = document.querySelector("#lock");
 const confirmBtn = document.querySelector("#confirmBtn");
-const library = document.querySelector("#library")
+const library = document.querySelector("#library");
 let bookcounter = 0;
 
 function book(){
@@ -22,26 +22,54 @@ function storeBook(book){
         bookStorage[bookStorage.length] = book;
 }
 
-function displayBooks(bookArray){
+function removeBook(btn){
+    let i = btn.id;
+    console.log(i);
+    let mainDiv = document.querySelector(`#div${i}`);
+    library.removeChild(mainDiv);  
+};
+
+function displayBooks(bookArray, indx){
     let mainDiv = document.createElement('div');
     mainDiv.style.width = "250px";
     mainDiv.style.height = "450px";
     mainDiv.style.backgroundColor = "red";
     mainDiv.style.margin = "25px"
     mainDiv.style.display = "grid";
-    mainDiv.style.gridTemplateRows = "350px 100px";
-    mainDiv.id = bookcounter;
+    mainDiv.style.gridTemplateRows = "repeat(5, auto)";
+    mainDiv.style.alignItems = "center";
+    mainDiv.style.justifyItems = "center";
+    mainDiv.id = `div${bookcounter}`;
 
-    let infotitle = document.createElement('p');
-    infotitle.textContent = `Title: ${bookArray[bookArray.length - 1].title}`;
+    let infoTitle = document.createElement('p');
+    infoTitle.textContent = `Title: ${bookArray[indx].title}`;
+
+    let infoAuthor = document.createElement('p');
+    infoAuthor.textContent = `Author: ${bookArray[indx].author}`;
+
+    let infoPages = document.createElement('p');
+    infoPages.textContent = `Pages: ${bookArray[indx].pages}`;
+
+    let infoRead = document.createElement('p');
+    infoRead.textContent = `Read: ${bookArray[indx].read}`;
 
     let removeBtn = document.createElement("button");
-    removeBtn.style.gridRowStart = "2";
+    removeBtn.style.gridRowStart = "5";
     removeBtn.style.margin = "20px";
+    removeBtn.textContent = "Remove";
+    removeBtn.id = bookcounter;
 
-    mainDiv.appendChild(infotitle);
+    removeBtn.addEventListener('click', () =>{
+        removeBook(removeBtn);
+    })
+
+    mainDiv.appendChild(infoTitle);
+    mainDiv.appendChild(infoAuthor);
+    mainDiv.appendChild(infoPages);
+    mainDiv.appendChild(infoRead);
     mainDiv.appendChild(removeBtn);
     library.appendChild(mainDiv);
+
 };
 
 function showForm(){
@@ -82,7 +110,7 @@ confirmBtn.addEventListener("click", () => {
     if(checkForm()){
         let newBook = new book();
         storeBook(newBook);
-        displayBooks(bookStorage);
+        displayBooks(bookStorage, bookStorage.length -1);
         hideForm();
     }else{
         alert("Either a value is empty or your pages entry is not a number");
